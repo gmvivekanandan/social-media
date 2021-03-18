a11b58c5ad9f58a790529e5755d1fb21ef5218a4
RahulKhannaMP <rkhannadp@gmail.com>
2021-03-18 21:39:26 +0530

    register screen

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import LoginScreen from "./app/screens/LoginScreen";
+import SignUpScreen from "./app/screens/SignUpScreen";
 
 export default function App() {
-  return <LoginScreen />;
+  return <SignUpScreen />;
 }
